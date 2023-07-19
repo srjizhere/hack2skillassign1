b059877455d203a1201857a5d5b9e9e9d63ce4b7
srjizhere <bhiapatil@gmail.com>
2023-07-19 16:49:57 +0530

    task2vackend

---
+++ assignment2/backend/controller/video.controller.js
@@ -0,0 +1,23 @@
+const { Video } = require("../model/video.model");
+
+const videoController = async (req, res) => {
+  try {
+    const page = req.query.page || 1;
+
+    const totalCount = await VideoModel.countDocuments();
+    const totalPage = Math.ceil(totalCount / 10);
+
+    const video = await VideoModel.find()
+      .sort({ publishedDate: -1 })
+      .skip((page - 1) * 10)
+      .limit(10);
+
+    res.send({ currentPage: page, totalPage: totalPage, video });
+  } catch (error) {
+    res.send(`error ${error.message}`);
+  }
+};
+
+module.exports = {
+    videoController
+}
